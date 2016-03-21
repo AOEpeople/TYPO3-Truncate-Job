6ad4e90113bb77be714e44d4e538c06b10ccf757
michael.sandritter <michael.sandritter@aoe.com>
2016-03-21 09:18:32 +0100

    added composer.json and connected repository with packagist. changed dependency constraint of typo3 from 4.7.0 to 4.7.0-6.2.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,11 +29,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'AOE GmbH',
-	'version' => '0.1.1',
+	'version' => '0.1.2',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.1.0-0.0.0',
-			'typo3' => '4.7.0-0.0.0',
+			'typo3' => '4.7.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
