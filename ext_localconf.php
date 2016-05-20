9386db86a3e436d5115bf36cdc460d3c6c901463
Juergen Kussmann <juergen.kussmann@aoe.com>
2016-05-20 15:24:01 +0200

    [FEATURE] use TYPO3 6.x-core-classes

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,8 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 if (TYPO3_MODE == 'BE') {
-	require_once t3lib_extMgm::extPath($_EXTKEY) . 'Classes/TruncateTask.php';
-	require_once t3lib_extMgm::extPath($_EXTKEY) . 'Classes/TruncateTaskFieldProvider.php';
+	require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/TruncateTask.php';
+	require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/TruncateTaskFieldProvider.php';
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Tx_TruncateJob_TruncateTask'] = array(
 		'extension' => $_EXTKEY,
 		'title' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_db.xml:scheduler_task.name',
