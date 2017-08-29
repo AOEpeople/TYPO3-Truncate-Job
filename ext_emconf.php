048cad37f0df0e74f1862f583072b9bbe247f06b
bilal.arslan <bilal.arslan@aoe.com>
2017-08-29 15:58:38 +0200

    [TASK] CD-63104 Added TYPO3v7 constraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.1.0-0.0.0',
-			'typo3' => '6.2.0-6.2.99',
+            'typo3' => '6.2.0-7.6.99',
+
 		),
 		'conflicts' => array(
 		),
